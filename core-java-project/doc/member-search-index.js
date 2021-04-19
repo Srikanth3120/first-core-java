6973d0aa893a5e99a3ab5ce2e955fc0461fd3ce5
Srikanth3120 <srikanth.nukala@yahoo.com>
2021-04-19 17:07:06 +0530

    first commit

---
+++ core-java-project/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.sapient.trg.pl","c":"HelloWorld","l":"HelloWorld()","url":"%3Cinit%3E()"},{"p":"com.sapient.trg.pl","c":"HelloWorld","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ core-java-project/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.sapient.trg.pl;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("today is"+new Date());
+		System.out.println("today "+Calendar.getInstance());
+
+	}
+
+}
